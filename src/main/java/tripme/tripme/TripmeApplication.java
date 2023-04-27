69731085418364066b781a12a4732c455c386c7c
jackson05 <jackson5g.bi@gmail.com>
2023-04-27 19:31:13 +0200

    initial code

---
+++ src/main/java/tripme/tripme/TripmeApplication.java
@@ -0,0 +1,13 @@
+package tripme.tripme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tripme/tripme/TripmeApplicationTests.java
@@ -0,0 +1,13 @@
+package tripme.tripme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
